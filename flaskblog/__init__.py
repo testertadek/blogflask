f1bdad5abc1a8822145aca92b9f195c9f1240871
testertadek <testertadek@users.noreply.github.com>
2020-06-05 19:57:01 +0000

    user authathication part1

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import email_validator
 
 
@@ -8,5 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '94256c982054236688cce428bcb32f3f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
